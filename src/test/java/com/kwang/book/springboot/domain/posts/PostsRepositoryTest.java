0ab8dfa4774a7a2cc111666b1f5d6d47aa8b2efd
KeiLights <shinkwang@yonsei.ac.kr>
2022-02-24 11:57:04 +0900

    현재 "게시글 등록" 화면 만들기 자바스크립트가 안되어서 잠시 멈춤 p143-144

---
+++ src/test/java/com/kwang/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.kwang.book.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() throws Exception {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("kwang")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void BaseTimeEntity_등록() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 2, 23, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("일기장")
+                .content("일기 쓰기 귀찮")
+                .author("kwang")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>>> createDate=" + posts.getCreateDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
---
+++ src/test/java/com/kwang/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,53 @@
+package com.kwang.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello가_리턴된다() throws Exception {
+        //given
+        String hello = "hello";
+        //when
+
+        //then
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+     }
+
+     @Test
+     void helloDto가_리턴된다() throws Exception {
+         //given
+         String name = "hello";
+         int amount = 1000;
+         //when
+
+         //then
+         mvc.perform(
+                         get("/hello/dto")
+                                 .param("name", name)
+                                 .param("amount", String.valueOf(amount)))
+                 .andExpect(status().isOk())
+                 .andExpect(jsonPath("$.name").value(name))
+                 .andExpect(jsonPath("$.amount").value(amount))
+                 .andExpect(jsonPath("$.amount", is(amount)));
+      }
+}
